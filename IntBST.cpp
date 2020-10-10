d7c78851f97f1b57479a48e2021ebb51b966abd7
Alie <aboondooley@gmail.com>
2020-10-09 19:12:36 -0700

    First commit, only .git ignore and partial header file.

---
+++ IntBST.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Alie on 10/9/2020.
+//
+
+#include "IntBST.h"
---
+++ IntBST.h
@@ -0,0 +1,47 @@
+//
+// Created by Alie on 10/9/2020.
+//
+
+#ifndef LAB4_INTBST_INTBST_H
+#define LAB4_INTBST_INTBST_H
+
+
+class IntBST {
+
+public:
+    IntBST();
+    ~IntBST();
+    IntBST &operator=(IntBST &rhs);
+    IntBST(const IntBST &other);
+    // TODO: rest of big 4
+    // FIXME: does the same!
+
+    bool has(int key) const;
+    void add(int newKey);
+    void remove(int key);
+
+private:
+    struct Node{
+        int key;
+        Node *left, *right;
+
+        // convenience constructor
+        Node(int k, Node *lch, Node *rch){
+            key = k;
+            left = lch; // always think of left before right
+            right = rch;
+        }
+        // there is no reason this is a method on the Node rather than on the
+        int findMax() const;
+    };
+    Node *root;
+    bool has(Node *me, int key) const;
+    Node *add(Node *me, int key);
+    Node *remove(Node *me, int key);
+    Node *copy(Node *me);
+    void clear(Node *me);
+
+};
+
+
+#endif //LAB4_INTBST_INTBST_H
