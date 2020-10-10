3b2f47dcae7b4b587c58b07f64a6bddb740e6bc3
Alie <aboondooley@gmail.com>
2020-10-10 14:07:45 -0700

    Added the first and second tests Which test the basic functions as well as empty.

--- IntBST.cpp
+++ IntBST.cpp
@@ -8,75 +8,12 @@ IntBST::IntBST() {
     root = nullptr;
 }
 
-bool IntBST::has(int key) const {
-    return has(root, key);
-}
-
-void IntBST::add(int newKey) {
-    root = add(root, newKey);
-}
-
-void IntBST::remove(int key) {
-    root = remove(root, key);
-
-}
-
-bool IntBST::empty() const {
-    if (root == nullptr) {
-        return true;
-    }
-    return false;
-}
-
-bool IntBST::has(IntBST::Node *me, int key) const {
-    if (me == nullptr)
-        return false;
-    else if (key < me->key)
-        return has(me->left, key);
-    else if (key > me->key)
-        return has(me->right, key);
-    else // key == me->key
-        return true;
-
-}
-// recursive helper
-IntBST::Node *IntBST::add(IntBST::Node *me, int newKey) {
-    if (me == nullptr)
-        // this is where I go!!
-        me = new Node(newKey, nullptr, nullptr);
-    else if (newKey < me->key)
-        me->left = add(me->left, newKey);
-    else if (newKey > me->key)
-        me->right = add(me->right, newKey);
-    // else equal - do nothing
-    return me;
-
-
-}
-
 IntBST::~IntBST() {
     clear(root); // delete the root and everything below
-
-}
-
-void IntBST::clear(IntBST::Node *me) {
-    if (me != nullptr)
-        clear(me->left);
-    clear(me->right);
-    delete me;
-    // basecase is nothing! The implied else - if the next is a
-
 }
 
 IntBST::IntBST(const IntBST &other) {
     root = copy(other.root);
-
-}
-
-IntBST::Node *IntBST::copy(IntBST::Node *me) {
-    if (me == nullptr)
-        return nullptr;
-    new Node(me->key, copy(me->left), copy(me->right));
 }
 
 IntBST &IntBST::operator=(IntBST &rhs) {
@@ -87,6 +24,81 @@ IntBST &IntBST::operator=(IntBST &rhs) {
     return *this;
 }
 
+bool IntBST::has(int key) const {
+    return has(root, key);
+}
+
+void IntBST::add(int newKey) {
+    root = add(root, newKey);
+}
+
+void IntBST::remove(int key) {
+    root = remove(root, key);
+}
+
+bool IntBST::empty() const {
+    if (root == nullptr) {
+        return true;
+    }
+    return false;
+}
+
+int IntBST::Node::findMax() const { // method of IntBST::Node
+   if (right == nullptr)
+        return key;
+    else
+        return right->findMax();
+}
+
+bool IntBST::Node::isLeaf() const { // also method of IntBST::Node
+    if (left == nullptr && right == nullptr){
+        return true;
+    }
+    return false;
+}
+
+void IntBST::clear(IntBST::Node *me) {
+    if (me != nullptr) {
+        clear(me->left);
+        clear(me->right);
+        delete me;
+    }
+    // base case is nothing! The implied else - if the next is a nullptr then
+    // we do nothing bc there is nothing to clear!
+}
+
+
+IntBST::Node *IntBST::copy(IntBST::Node *me) {
+    if (me == nullptr) {
+        return nullptr;
+    }
+    new Node(me->key, copy(me->left), copy(me->right));
+}
+
+bool IntBST::has(IntBST::Node *me, int key) {
+    if (me == nullptr)
+        return false;
+    if (key < me->key)
+        return has(me->left, key);
+    if (key > me->key)
+        return has(me->right, key);
+    // key == me->key
+        return true;
+}
+// recursive helper
+IntBST::Node *IntBST::add(IntBST::Node *me, int newKey) {
+    if (me == nullptr)
+        // this is where I go!!
+        me = new Node(newKey, nullptr, nullptr);
+    else if (newKey < me->key)
+        me->left = add(me->left, newKey);
+    else if (newKey > me->key)
+        me->right = add(me->right, newKey);
+    // else equal (key already in set) - do nothing
+    return me;
+}
+
+
 IntBST::Node *IntBST::remove(IntBST::Node *me, int key) {
     if (me == nullptr)
         return nullptr; // nothing to remove, key not found
@@ -96,36 +108,25 @@ IntBST::Node *IntBST::remove(IntBST::Node *me, int key) {
     } else if (key > me->key) {
         me->right = remove(me->right, key);
         return me;
-    } else
-        // FOUND IT!
-    if (me->left == nullptr) {
-        Node *myReplacement = me->right;
-        delete me;
-        return myReplacement;
-    } else if (me->right == nullptr){
-        Node *myReplacement = me->left;
-        delete me;
-        return myReplacement;
     } else {
-        // replace me with max element in my left subtree
-        me->key = me->left->findMax();
-        me->left = remove(me->left, me->key);
+        // FOUND IT!
+        if (me->left == nullptr) {
+            Node *myReplacement = me->right;
+            delete me;
+            return myReplacement;
+        } else if (me->right == nullptr) {
+            Node *myReplacement = me->left;
+            delete me;
+            return myReplacement;
+        } else {
+            // replace me with max element in my left subtree
+            me->key = me->left->findMax();
+            me->left = remove(me->left, me->key);
+        }
     }
 
 }
 
 
 
-int IntBST::Node::findMax() const { // notice that it is within intBST and Node
-    if (right == nullptr)
-        return key;
-    else
-        return right->findMax();
-}
 
-bool IntBST::Node::isLeaf() const {
-    if (left == nullptr && right == nullptr){
-        return true;
-    }
-    return false;
-}
--- lab4.cpp
+++ lab4.cpp
@@ -1,6 +1,58 @@
 #include <iostream>
+#include "IntBST.h"
+using namespace std;
+
+bool first_test(){
+    string title = "first test: ";
+
+    IntBST bst;
+    cout << title << "check if empty tree has 1" << endl;
+    if (bst.has(1))
+        return false;
+
+    cout << title << "add 1 and check tree has 1" << endl;
+    bst.add(1);
+    IntBST bst2(bst); // create copy to check later
+    if (!bst.has(1))
+        return false;
+
+    cout << title << "remove 1 and check tree has 1" << endl;
+    bst.remove(1);
+    if (bst.has(1))
+        return false;
+
+    cout << title << "check if copy has 1" << endl;
+    if (!bst2.has(1))
+        return false;
+
+    cout << title << "check assigned version has 1" << endl;
+    bst = bst2;
+    if (!bst.has(1))
+        return false;
+
+    cout << title;
+    return true;
+}
+
+bool second_test(){
+    string title = "second test: ";
+    IntBST bst;
+    cout << title << "check if empty tree is empty";
+    if (!bst.empty())
+        return false;
+
+    cout << title << "add 10 and check if tree is empty";
+    bst.add(10);
+    if (bst.empty())
+        return false;
+
+    cout << title;
+    return true;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << (first_test() ? "passed" : "failed") << endl;
+    cout << (second_test() ? "passed" : "failed") << endl;
+
     return 0;
 }
